280db0dbef4e567e19f66c17614cb1c83b685f9f
Oh seung min <ohmin@gmail.com>
2024-03-14 23:05:42 +0900

    변수와 객체를 타입스크립트로 작성 그리고 인터페이스 작성해보기

---
+++ test.ts
@@ -0,0 +1,15 @@
+interface Person {
+  name : string;
+  age : number;
+}
+
+const value : string = "문장";
+console.log(value);
+
+const obj : {name : string, age : number} = {
+  name:  "홍길동",
+  age: 20
+}
+
+console.log(obj);
+console.log(obj.name);
