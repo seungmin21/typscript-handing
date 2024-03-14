a561d616b8d3c08d27500964e7e4cdc45afd71c3
Oh seung min <ohmin@gmail.com>
2024-03-14 23:08:38 +0900

    타입스크립트로 함수 작성하기

--- test.ts
+++ test.ts
@@ -13,3 +13,13 @@ const obj : {name : string, age : number} = {
 
 console.log(obj);
 console.log(obj.name);
+
+function add (x : string , y : string) {
+  return x + y;
+}
+
+const insa : string = "hello";
+const basic : string = " world!";
+const result : string = add(insa, basic);
+
+console.log(result);
